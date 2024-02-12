6ee4390ed13b8a0da7e28a4ce2a25c312bfc7f2e
cristianoronaldoo7 <81501167+cristianoronaldoo7@users.noreply.github.com>
2024-02-12 12:21:04 +0530

    Add files via upload

---
+++ Notekeeper/src/js/db.js
@@ -0,0 +1,151 @@
+/**
+ * @copyright codewithsadee 2023
+ */
+
+'use strict';
+
+import { generateID,findNotebook, findNotebookIndex,findNote , findNoteIndex} from "./utils.js";
+
+//db object
+ let notekeeperDB = {};
+
+ const initDB = function () {
+  const  db = localStorage.getItem('notekeeperDB');
+
+  if(db){
+    notekeeperDB = JSON.parse(db);
+
+  }
+  else{
+    notekeeperDB.notebooks = [];
+    localStorage.setItem('notekeeperDB',JSON.stringify(notekeeperDB));
+
+  }
+
+ }
+
+ initDB();
+
+  const readDB = function () {
+    notekeeperDB = JSON.parse(localStorage.getItem('notekeeperDB'));
+  }
+
+  const writeDB = function () {
+    localStorage.setItem('notekeeperDB',JSON.stringify(notekeeperDB));
+  }
+
+
+ export const db = {
+   post: {
+    notebook(name){
+        readDB();
+        const notebookData = {
+            id : generateID(),
+            name,
+            notes: []
+        }
+
+        notekeeperDB.notebooks.push(notebookData);
+        writeDB();
+        return notebookData;
+
+    },
+    note(notebookId,object){
+      readDB();
+      
+      const notebook = findNotebook(notekeeperDB,notebookId);
+      const noteData = {
+        id: generateID(),
+        notebookId,
+        ...object,
+        postedOn: new Date().getTime()
+
+
+      }
+         
+      notebook.notes.unshift(noteData);
+
+      writeDB();
+
+      return noteData;
+    }
+   },
+
+   get: {
+
+    notebook(){
+        readDB();
+
+        return notekeeperDB.notebooks;
+    },
+
+    note(notebookId){
+      readDB();
+
+      const notebook = findNotebook(notekeeperDB, notebookId);
+      return notebook.notes;
+
+
+    }
+   },
+
+   update: {
+
+    notebook(notebookId , nam){
+        readDB();
+         
+        const notebook = findNotebook(notekeeperDB,notebookId);
+        notebook.name = nam;
+       
+
+        writeDB();
+
+        return notebook;
+
+
+    },
+
+    note(noteId ,object ){
+      readDB();
+      const oldNote = findNote(notekeeperDB,noteId);
+
+      const newNote= Object.assign(oldNote,object);
+
+      writeDB();
+
+      return newNote;
+    }
+   },
+
+   delete:{
+
+    notebook(notebookId){
+      readDB();
+       
+      const notebookIndex = findNotebookIndex(notekeeperDB,notebookId);
+      notekeeperDB.notebooks.splice(notebookIndex,1);
+
+
+
+      writeDB();
+
+    },
+
+    note(notebookId,noteId){
+      readDB();
+       
+      const notebook = findNotebook(notekeeperDB,notebookId);
+      const noteIndex = findNoteIndex(notebook , noteId);
+
+      notebook.notes.splice(noteIndex,1);
+
+
+
+      writeDB();
+      return notebook.notes;
+      
+    }
+   }
+
+
+ }
---
+++ Notekeeper/src/js/theme.js
@@ -0,0 +1,33 @@
+/**
+ * @copyright codewithsadee 2023
+ */
+
+'use strict';
+
+const toggleTheme = function () {
+    const currentTheme = document.documentElement.getAttribute('data-theme')
+     || 'light';
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme',newTheme);
+    localStorage.setItem('theme',newTheme);
+}
+
+
+const storedTheme = localStorage.getItem('theme');
+console.log("🚀 ~ storedTh̥eme:", storedTheme)
+
+const systemThemeIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const initialTheme = storedTheme ?? 
+(systemThemeIsDark ?'dark' : 'light' );
+
+document.documentElement.setAttribute('data-theme',initialTheme);
+
+
+window.addEventListener('DOMContentLoaded',function() {
+    const $themeBtn = document.querySelector('[data-theme-btn]');
+    if($themeBtn) $themeBtn.addEventListener('click',toggleTheme);
+});
+
+
